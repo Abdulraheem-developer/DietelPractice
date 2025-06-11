692017b7db08fabd23207da554ca0e4d4de8ac8c
Abdulraheem <eremirogers@gmail.com>
2025-06-11 01:24:56 +0100

    Methods with two parameters practice

---
+++ TwoParameters.java
@@ -0,0 +1,19 @@
+public class TwoParameters{
+
+	public static void makeBreakFast(String food, int quantity){
+	
+	System.out.println("Making " + quantity + " plates of " + food);
+
+	}
+
+
+public static void main(String[] args){
+
+	makeBreakFast("Eba", 2);
+	makeBreakFast("Spaghetti", 3);
+	makeBreakFast("Rice", 4);
+}
+
+
+
+}
