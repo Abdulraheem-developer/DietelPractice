353fbdd8751cee74889b43b7a1975ea88d7600dd
Abdulraheem <eremirogers@gmail.com>
2025-06-12 22:25:30 +0100

    Improved Multiplication Table

---
+++ ImprovedMultiplicationTable.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ImprovedMultiplicationTable{
+ public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Enter a range for the multiplication table: ");
+	int n = input.nextInt();
+	
+	for(int num = 1; num <= n; num++){
+	
+	 System.out.println("\nMultiplication Table for: " + num);
+
+	for(int i = 1; i <= 12; i++){
+	System.out.println(num + " x " + i + " = " + (num * i));
+	
+	}
+	
+	}	
+	
+	
+}
+}
