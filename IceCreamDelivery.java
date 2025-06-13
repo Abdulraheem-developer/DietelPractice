306bef15d2dc5bbbd52fdcbd3a0b1b4e011c8de9
Abdulraheem <eremirogers@gmail.com>
2025-06-13 07:44:00 +0100

    Ice Cream Delivery with Nested Loops

---
+++ IceCreamDelivery.java
@@ -0,0 +1,17 @@
+public class IceCreamDelivery{
+ public static void main(String[] args){
+
+	for(int street = 1; street <= 5; street++){
+	  System.out.println("Going to Street #" + street);
+	
+		for(int house = 1; house <= 3; house++){
+		System.out.println("Delivered ice-cream to house #" + house + " on Street #" + street );
+		}
+	
+			System.out.println();
+	}
+
+
+}
+
+}
